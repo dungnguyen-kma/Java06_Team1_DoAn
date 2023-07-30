d2ffd19733ae9e79b84cb446b8b2c18fc83279a8
dungnguyen-kma <dungnguyen.kma@gmail.com>
2023-07-30 18:46:32 +0700

    khoi tao do an cuoi khoa java06 team 1

---
+++ src/main/java/com/actvn/java06/Java06_Team1_DoAn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.actvn.java06;
+
+/**
+ *
+ * @author thepo
+ */
+public class Java06_Team1_DoAn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
